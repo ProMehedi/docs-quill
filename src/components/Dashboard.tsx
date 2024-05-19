e6f13e2346d15fcb798fe66ad1258c919a8481da
promehedi <admin@promehedi.com>
2024-05-19 16:06:35 +0600

    Add getUserFiles query in trpc

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,7 +1,13 @@
+'use client'
 import React from 'react'
 import UploadButton from './UploadButton'
+import { trpc } from '~/app/_trpc/client'
 
 const Dashboard = () => {
+  const { data: files } = trpc.getUserFiles.useQuery()
+
+  console.log(files)
+
   return (
     <main className='mx-auto max-w-7xl md:p-10'>
       <div className='mt-8 flex flex-col items-start justify-between gap-4 border-b border-gray-200 pb-5 sm:flex-row sm:items-center sm:gap-0'>
--- src/trpc/trpc.ts
+++ src/trpc/trpc.ts
@@ -1,6 +1,21 @@
-import { initTRPC } from '@trpc/server'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import { initTRPC, TRPCError } from '@trpc/server'
 
 const t = initTRPC.create()
 
+const isAuth = t.middleware(async (opts) => {
+  const { getUser } = getKindeServerSession()
+  const user = getUser()
+
+  if (user === undefined || !user.id || !user.email) {
+    throw new TRPCError({ code: 'UNAUTHORIZED' })
+  }
+
+  return opts.next({
+    ctx: { userId: user.id, user },
+  })
+})
+
 export const router = t.router
 export const publicProcedure = t.procedure
+export const privateProcedure = t.procedure.use(isAuth)
